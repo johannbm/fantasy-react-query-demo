5ec51453581f57dfa5ffce76589aa6bb436a27a0
Johannes <johannes.moskvil@knowit.no>
2021-02-21 15:05:55 +0100

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
-function App() {
+
+
+export default function App() {
+  const [loading, setLoading]  = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(async () => {
+    setLoading(true);
+    const data = await fetchStats();
+    setData(data);
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {data.map(d => {
+          <div>{d}</div>
+        })}
       </header>
     </div>
   );
 }
 
-export default App;
+const API_URL = "https://3hg93n5vqg.execute-api.eu-north-1.amazonaws.com/test/league-players";
+async function fetchStats() {
+  const res = await fetch(API_URL);
+  console.log(res);
+  return res.json();
+}
